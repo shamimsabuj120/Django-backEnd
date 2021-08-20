0f3900ad0b82f24f8ffc30024d1260f4e588ded2
shamim sabuj <76744826+shamimsabuj120@users.noreply.github.com>
2021-08-20 21:08:29 +0600

    third day done

--- my_first_project/my_first_project/urls.py
+++ my_first_project/my_first_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
+from django.urls import path
+from first_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+    path('home/', views.index, name='home'),
+    path('', views.index, name='index'),
+
 ]
