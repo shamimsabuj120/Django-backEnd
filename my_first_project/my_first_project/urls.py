f36fbd0c54b48a3ee11f608fd56ee88a10f6c558
shamim sabuj <76744826+shamimsabuj120@users.noreply.github.com>
2021-08-23 18:35:47 +0600

    fifth day

--- my_first_project/my_first_project/urls.py
+++ my_first_project/my_first_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import include
 # from first_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app/',include('first_app.urls')),
+    path('',include('first_app.urls')),
     # path('', views.index, name='index'),
     # path('contact/',views.contact, name='contact'),
     # path('about/', views.about, name='about')
