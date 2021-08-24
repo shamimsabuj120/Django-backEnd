d116e86a07c5128c2408db05e697646e19cd3865
shamim sabuj <76744826+shamimsabuj120@users.noreply.github.com>
2021-08-25 01:52:43 +0600

    6th day

--- my_first_project/first_app/admin.py
+++ my_first_project/first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from first_app.models import musician, album
 
 # Register your models here.
+admin.site.register(musician)
+admin.site.register(album)
--- my_first_project/first_app/models.py
+++ my_first_project/first_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class musician(models.Model):
+    #id =models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class album(models.Model):
+    # id =models.AutoField(primary_key=True)
+    artist = models.ForeignKey(musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    release_date = models.DateField()
+    num_starts = models.IntegerField()
+
+def __str__(self):
+    return self.first_name+ " " + self.last_name
